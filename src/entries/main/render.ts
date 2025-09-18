0fcba4a606f39da6d1aa3f270fc0761b939bcd46
axtk <axtkx@inbox.ru>
2025-09-18 12:28:36 +0300

    add styles file

--- src/entries/main/render.ts
+++ src/entries/main/render.ts
@@ -1,21 +1,17 @@
 import type {Controller} from '@t8/webapp-core';
 
 export const render: Controller = () => {
-    return (req, res) => {
-        let {nonce} = req.ctx;
-
+    return (_req, res) => {
         res.send(
             '<!DOCTYPE html>' +
             '<html><head><meta charset="utf-8">' +
             '<meta name="viewport" content="width=device-width">' +
+            '<title>Blank App</title>' +
+            '<link rel="stylesheet" href="/_main/index.css">' +
             '<link rel="icon" type="image/svg+xml" href="/favicon.svg">' +
-            '<title>Intro</title>' +
-            `<style${nonce ? ` nonce="${nonce}"` : ''}>` +
-            '.layout { max-width: 36em; margin: 0 auto; }</style>' +
             '</head><body><div class="layout">' +
-            '<main><h1>Intro</h1>' +
-            '<p>This is demo content. Lorem ipsum dolor, ' +
-            'quam velit, tincidunt vitae suscipit nullam.</p>' +
+            '<main><h1>Blank App</h1>' +
+            '<p>Lorem ipsum dolor tincidunt vitae suscipit nullam</p>' +
             '</main><footer><hr><p><em>@t8/blank-app</em></p></footer>' +
             '</div></body></html>\n',
         );
--- src/entries/main/server.ts
+++ src/entries/main/server.ts
@@ -1,6 +1,7 @@
-import {Router} from 'express';
+import {Router, static as expressStatic} from 'express';
 import {render} from './render';
 
 export let server = Router();
 
+server.use('/_main/', expressStatic('src/entries/main/public'));
 server.get('/', render());

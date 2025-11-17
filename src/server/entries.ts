c12cf6a331885f28d78bedf021f32c53498ae6dd
axtk <axtkx@inbox.ru>
2025-11-17 17:26:43 +0300

    lint

--- src/server/entries.ts
+++ src/server/entries.ts
@@ -1,5 +1,7 @@
 // Populated automatically during the build phase
-export const entries = (await Promise.all([
-  // main
-  import("../entries/main/server.ts"),
-])).map(({ server }) => server);
+export const entries = (
+  await Promise.all([
+    // main
+    import("../entries/main/server.ts"),
+  ])
+).map(({ server }) => server);
